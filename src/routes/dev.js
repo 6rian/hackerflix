841b39242bc3b28060a2c8f13e9d92b91ee4e545
Brian Griffin <wbgriffin@pm.me>
2022-08-27 12:18:01 -0400

    feat: add TMDB service

--- src/routes/dev.js
+++ src/routes/dev.js
@@ -3,10 +3,31 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.use('/tmdb', (req, res, next) => {
+router.use('/tmdb/list', async (req, res, next) => {
   try {
     const tmdb = new TMDB();
-    res.json({ success: true });
+    const list = await tmdb.getList('8214827');
+    res.json({ data: list });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.use('/tmdb/movie', async (req, res, next) => {
+  try {
+    const tmdb = new TMDB();
+    const movie = await tmdb.getMovie('10428');
+    res.json({ data: movie });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.use('/tmdb/show', async (req, res, next) => {
+  try {
+    const tmdb = new TMDB();
+    const show = await tmdb.getShow('59659');
+    res.json({ data: show });
   } catch (e) {
     next(e);
   }
--- src/services/tmdb.js
+++ src/services/tmdb.js
@@ -1,15 +1,37 @@
 import config from 'config';
+import fetch from 'node-fetch';
+import logger from '../lib/logger';
 
 export default class TMDB {
   constructor() {
     try {
       this.ACCESS_TOKEN = config.get('tmdb.access_token');
+      this.API_URL = config.get('tmdb.api_url');
     } catch (e) {
       throw new Error('Missing TMDB Access Token');
     }
   }
 
-  #request() {
-    console.log('private!');
+  async #request(url) {
+    logger.info(`TMDB: Requesting ${url}`);
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: `Bearer ${this.ACCESS_TOKEN}`,
+      },
+    });
+    return await response.json();
+  }
+
+  async getList(id) {
+    return await this.#request(`${this.API_URL}list/${id}`);
+  }
+
+  async getMovie(id) {
+    return await this.#request(`${this.API_URL}movie/${id}`);
+  }
+
+  async getShow(id) {
+    return await this.#request(`${this.API_URL}tv/${id}`);
   }
 }
