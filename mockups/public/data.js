2eed0e970560d99b68ad0cd02a32c7699aeeba74
Brian Griffin <wbgriffin@pm.me>
2022-08-27 23:02:48 -0400

    feat: more homepage mock updates

--- mockups/public/data.js
+++ mockups/public/data.js
@@ -10,5 +10,37 @@ const results = [
   {
     "title": "Mr. Robot",
     "poster": "https://www.themoviedb.org/t/p/original/qE0t9rlClIReax0d5tr3j300wUt.jpg",
+  },
+  {
+    "title": "Zero Days",
+    "poster": "https://www.themoviedb.org/t/p/original/xwQVkRJGCH59jGNPR78Aw8wzyZg.jpg",
+  },
+  {
+    "title": "We Are Legion",
+    "poster": "https://www.themoviedb.org/t/p/original/6h3KVwIyPCh1pzS4mchjuUBgZ17.jpg",
+  },
+  {
+    "title": "TPB AFK: The Pirate Bay Away from Keyboard",
+    "poster": "https://www.themoviedb.org/t/p/original/3W2PYp6alnzCqhAzGt9Xs4iMkK4.jpg",
+  },
+  {
+    "title": "AlphaGo",
+    "poster": "https://www.themoviedb.org/t/p/original/mBebKFGVVuv6tDnUP8F5wKqxuNA.jpg",
+  },
+  {
+    "title": "The Social Dilemma",
+    "poster": "https://www.themoviedb.org/t/p/original/jcaM6V2tCtu6iMHDsGLBUbaYgYp.jpg",
+  },
+  {
+    "title": "The Great Hack",
+    "poster": "https://www.themoviedb.org/t/p/original/uIrGgUzasEUA7v8uChRhUUFzLwE.jpg"
+  },
+  {
+    "title": "Don't F**k with Cats: Hunting an Internet Killer",
+    "poster": "https://www.themoviedb.org/t/p/original/Crc4XkhLddMTNJfj1iLca0w1Bb.jpg",
+  },
+  {
+    "title": "The Billion Dollar Code",
+    "poster": "https://www.themoviedb.org/t/p/original/9EQLnJp0aBuuBdudJ67osS7Bicq.jpg",
   }
 ];
