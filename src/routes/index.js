6c021060ae0c72cd6a74b7a7f70106856fbcfa8c
Brian Griffin <wbgriffin@pm.me>
2022-09-07 09:31:49 -0400

    feat: add type filter to API call from index template

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,36 @@
 import { Router } from 'express';
 import apiRoutes from './api';
 import devRoutes from './dev';
+import { isValidMediaType } from '../lib/utils';
 
 const router = Router();
 
 router.use('/api', apiRoutes);
 router.use('/dev', devRoutes);
 
+const GLOBAL_PAGE_PARAMS = {
+  title: 'HackerFlix.net',
+};
+
 router.get('/', (req, res, next) => {
-  res.render('index', {title: 'HackerFlix.net'});
+  res.render('index', GLOBAL_PAGE_PARAMS);
 });
 
 router.get('/flick/:slug', (req, res, next) => {
-  res.render('flick', { title: 'HackerFlix.net' });
+  res.render('flick', GLOBAL_PAGE_PARAMS);
+});
+
+router.get('/browse/:type', (req, res, next) => {
+  try {
+    const { type } = req.params;
+    if (!type || !isValidMediaType(type)) {
+      throw new Error(`Invlid media type: ${type}`);
+    }
+
+    res.render('index', { ...GLOBAL_PAGE_PARAMS, mediaType: type });
+  } catch (e) {
+    next(e);
+  }
 });
 
 export default router;
