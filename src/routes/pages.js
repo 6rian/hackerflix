7aab26721bd9b0c2e2d9ab19653a4687e9ea6a2f
Brian Griffin <wbgriffin@pm.me>
2022-09-11 11:11:37 -0400

    feat: SSR flick view with data

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
 import { isValidMediaType } from '../lib/utils';
+import { getFlickDTO } from '../dtos/flick';
+import { runDetailsETL } from '../etls/tmdb/details';
+import * as cache from '../services/cache';
 
 const router = Router();
 
@@ -20,13 +23,42 @@ router.get('/', (req, res, next) => {
   res.render('index', locals);
 });
 
-router.get('/flick/:slug', (req, res, next) => {
-  const view = 'flick';
-  const locals = makeLocals({
-    view,
-    slug: req.params.slug,
-  });
-  res.render(view, locals);
+router.get('/flick/:slug', async (req, res, next) => {
+  try {
+    const { slug } = req.params;
+    const flick = await cache.getFlickBySlug(slug);
+    let flickData;
+
+    if (!flick) {
+      throw new Error(`Flick not found for slug: ${slug}`);
+    }
+
+    if (!flick.hasAllDetails) {
+      const id = flick.tmdbId;
+      const type = flick.tmdbMediaType;
+
+      const detailsLoaded = await runDetailsETL(id, type);
+
+      if (!detailsLoaded) {
+        throw new Error(`Failed loading details for slug: ${slug}`);
+      }
+
+      const updatedFlick = await cache.getFlickById(id, type);
+      flickData = getFlickDTO(updatedFlick);
+    } else {
+      flickData = getFlickDTO(flick);
+    }
+
+    const view = 'flick';
+    const locals = makeLocals({
+      view,
+      slug: req.params.slug,
+      flick: flickData,
+    });
+    res.render(view, locals);
+  } catch (e) {
+    next(e);
+  }
 });
 
 router.get('/browse/:type', (req, res, next) => {
